d9a24d406306446e774b47e1b2a799a63e59294b
denis_ivanuchenko <dennisivanuchenko@gmail.com>
2021-11-28 12:34:27 +0300

    Добавлена реализация поиска занятия по адрессу преподавателя

--- index.php
+++ index.php
@@ -70,6 +70,11 @@ if('/lesson'===$_SERVER['PATH_INFO'])      // Поиск занятия. [нач
             $LessonMeetSearchCriteria=($_GET['teacher_phone']===$TeachersIdToInfo[$lesson['teacher_id']]['phone']);
         }// Поиск по присутвию teacher_phone в GET запросе и совпадению teacher_phone в запросе и массиве занятий. [конец]
 
+        if(array_key_exists('teacher_address',$_GET)) // Поиск по присутвию teacher_address в GET запросе и совпадению teacher_address в запросе и массиве занятий. [начало]
+        {
+            $LessonMeetSearchCriteria=($_GET['teacher_address']===$TeachersIdToInfo[$lesson['teacher_id']]['address']);
+        }// Поиск по присутвию teacher_address в GET запросе и совпадению teacher_address в запросе и массиве занятий. [конец]
+
         if(array_key_exists('class_id',$_GET)&&(int)$_GET['class_id']===$lesson['class_id'])// Поиск по присутвию class_id в GET запросе и совпадению class_id занятия в запросе и массиве занятий. [начало]
         {
             $result[]=$lesson;

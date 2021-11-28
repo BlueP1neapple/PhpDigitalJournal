509424e7c863fc80f3980e27ad633d7b6a79579e
denis_ivanuchenko <dennisivanuchenko@gmail.com>
2021-11-28 10:56:33 +0300

    Добавлен тест поиска занятия по id занятия. Реализован поиск занятия по id занятия.

--- index.php
+++ index.php
@@ -3,19 +3,22 @@ $PathToLesson = __DIR__ .'\JSON/lesson.json';
 $LessonTXT=file_get_contents($PathToLesson);
 $Lessons=json_decode($LessonTXT,true);  // Перевод lesson.json в php массив
 
-if('/lesson'===$_SERVER['PATH_INFO'])      //Поиск занятия по id предмета. [начало]
+if('/lesson'===$_SERVER['PATH_INFO'])      // Поиск занятия. [начало]
 {
     $httpCode=200;
     $result=[];
-    foreach($Lessons as $lesson)// //Цикл по все занятиям. [начало]
+    foreach($Lessons as $lesson) // Цикл по все занятиям. [начало]
     {
-        if(array_key_exists('item_id',$_GET)&&(int)$_GET['item_id']===$lesson['item_id']) //Поиск по присутвию item_id в GET запросе и совпадению id предмета в запросе и массиве занятий. [начало]
+        if(array_key_exists('item_id',$_GET)&&(int)$_GET['item_id']===$lesson['item_id']) // Поиск по присутвию item_id в GET запросе и совпадению id предмета в запросе и массиве занятий. [начало]
         {
             $result[]=$lesson;
-            break;
-        } //Поиск по присутвию item_id в GET запросе и совпадению id предмета в запросе и массиве занятий. [конец]
-    }  // //Цикл по все занятиям. [конец]
-} //Поиск занятия по id предмета. [конец]
+        } // Поиск по присутвию item_id в GET запросе и совпадению id предмета в запросе и массиве занятий. [конец]
+        if(array_key_exists('lesson_id',$_GET)&&(int)$_GET['lesson_id']===$lesson['id'])// Поиск по присутвию lesson_id в GET запросе и совпадению id занятия в запросе и массиве занятий. [начало]
+        {
+            $result[]=$lesson;
+        }// Поиск по присутвию lesson_id в GET запросе и совпадению id занятия в запросе и массиве занятий. [начало]
+    }  //Цикл по все занятиям. [конец]
+} // Поиск занятия. [конец]
 
 header('Content-type: application/json');
 http_response_code($httpCode);

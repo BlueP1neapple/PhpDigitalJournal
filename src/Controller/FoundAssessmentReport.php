eb2747386c483b0129eb2b75441c8e27cbe2e54d
Денис <ivanyuchenko.d.o@effective-group.ru>
2021-12-22 10:19:05 +0300

    Класс поиска оценок сделан

--- src/Controller/FoundAssessmentReport.php
+++ src/Controller/FoundAssessmentReport.php
@@ -24,15 +24,44 @@ use function JoJoBizzareCoders\DigitalJournal\Infrastructure\paramTypeValidation
  */
 final class FoundAssessmentReport
 {
+    // Свойства
+    /**
+     * Использвуемый логгер
+     *
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
+     * Используемый конфиг приложения
+     *
+     * @var AppConfig
+     */
+    private AppConfig $appConfig;
+
+    //Методы
+
+    /**
+     * Конструктор поиска Оценок
+     *
+     * @param LoggerInterface $logger - используемый логгер
+     * @param AppConfig $appConfig - Конфиг приложения
+     */
+    public function __construct(LoggerInterface $logger, AppConfig $appConfig)
+    {
+        $this->logger = $logger;
+        $this->appConfig = $appConfig;
+    }
+
+
     /**
      * Загрузка данных о предметах и создаёт сущности Предметов на основе этих данных
      *
-     * @param AppConfig $appConfig - конфиг приложения
      * @return array
      */
-    private function loadItemsEntity(AppConfig $appConfig): array
+    private function loadItemsEntity(): array
     {
-        $items = loadData($appConfig->getPathToItems());
+        $items = loadData($this->appConfig->getPathToItems());
         $itemsIdToInfo = [];
         foreach ($items as $item) {
             $itemsObj = ItemClass::createFromArray($item);
@@ -44,13 +73,12 @@ final class FoundAssessmentReport
     /**
      * Загрузка данных о учителях и создаёт сущности Учителя на основе этих данных
      *
-     * @param AppConfig $appConfig - конфиг приложения
      * @param array $itemsIdToInfo - Сущности Предметов
      * @return array
      */
-    private function loadTeachersEntity(AppConfig $appConfig, array $itemsIdToInfo): array
+    private function loadTeachersEntity(array $itemsIdToInfo): array
     {
-        $teachers = loadData($appConfig->getPathToTeachers());
+        $teachers = loadData($this->appConfig->getPathToTeachers());
         $teachersIdToInfo = [];
         foreach ($teachers as $teacher) {
             $teacher['idItem'] = $itemsIdToInfo[$teacher['idItem']];
@@ -63,12 +91,11 @@ final class FoundAssessmentReport
     /**
      * Загрузка данных о классах и создаёт сущности Классы на основе этих данных
      *
-     * @param AppConfig $appConfig - конфиг приложения
      * @return array
      */
-    private function loadClassEntity(AppConfig $appConfig): array
+    private function loadClassEntity(): array
     {
-        $classes = loadData($appConfig->getPathToClasses());
+        $classes = loadData($this->appConfig->getPathToClasses());
         $classesIdToInfo = [];
         foreach ($classes as $class) {
             $classesObj = ClassClass::createFromArray($class);
@@ -80,19 +107,15 @@ final class FoundAssessmentReport
     /**
      * Загрузка данных о занятиях и создаёт сущности Занятия на основе этих данных
      *
-     * @param AppConfig $appConfig - конфиг приложения
      * @param array $itemsIdToInfo - сущности Предметов
-     * @param array $teachersIdToInfo - сущности Учителя
-     * @param array $classesIdToInfo - Сущности Классы
      * @return array
      */
     private function loadLessonEntity(
-        AppConfig $appConfig,
-        array $itemsIdToInfo,
-        array $teachersIdToInfo,
-        array $classesIdToInfo
+        array $itemsIdToInfo
     ): array {
-        $lessons = loadData($appConfig->getPathToLesson());
+        $lessons = loadData($this->appConfig->getPathToLesson());
+        $teachersIdToInfo = $this->loadTeachersEntity($itemsIdToInfo);
+        $classesIdToInfo = $this->loadClassEntity();
         $lessonIdToInfo = [];
         foreach ($lessons as $lesson) {
             $lesson['item_id'] = $itemsIdToInfo[$lesson['item_id']];
@@ -107,23 +130,21 @@ final class FoundAssessmentReport
     /**
      * Метод реализующий загрузку данных о Оцкенках
      *
-     * @param AppConfig $appConfig - конфиг приложения
      * @return array
      */
-    private function loadReportData(AppConfig $appConfig): array
+    private function loadReportData(): array
     {
-        return loadData($appConfig->getPathToAssessmentReport());
+        return loadData($this->appConfig->getPathToAssessmentReport());
     }
 
     /**
      * Загрузка данных о родителях и создаёт сущности Родители на основе этих данных
      *
-     * @param AppConfig $appConfig - конфиг приложения
      * @return array
      */
-    private function loadParentsEntity(AppConfig $appConfig): array
+    private function loadParentsEntity(): array
     {
-        $parents = loadData($appConfig->getPathToParents());
+        $parents = loadData($this->appConfig->getPathToParents());
         $parentIdToInfo = [];
         foreach ($parents as $parent) {
             $parentsObj = ParentUserClass::createFromArray($parent);
@@ -135,14 +156,13 @@ final class FoundAssessmentReport
     /**
      * Загрузка данных о Учениках и создаёт сущности Ученики на основе этих данных
      *
-     * @param AppConfig $appConfig - конфиг приложения
-     * @param array $classesIdToInfo - сущности Классы
-     * @param array $parentIdToInfo - сущности Родители
      * @return array
      */
-    private function loadStudentsEntity(AppConfig $appConfig, array $classesIdToInfo, array $parentIdToInfo): array
+    private function loadStudentsEntity(): array
     {
-        $students = loadData($appConfig->getPathToStudents());
+        $students = loadData($this->appConfig->getPathToStudents());
+        $classesIdToInfo = $this->loadClassEntity();
+        $parentIdToInfo = $this->loadParentsEntity();
         $studentIdToInfo = [];
         foreach ($students as $student) {
             $student['class_id'] = $classesIdToInfo[$student['class_id']];
@@ -187,7 +207,6 @@ final class FoundAssessmentReport
      * Поиск оценки
      *
      * @param ServerRequest $serverRequest - серверный http запрос
-     * @param LoggerInterface $logger - логгер
      * @param array $reports - массив данных оценок
      * @param array $itemsIdToInfo - сущность Предметов
      * @param array $lessonIdToInfo - массив сущностей Занятий
@@ -196,7 +215,6 @@ final class FoundAssessmentReport
      */
     private function searchForAssessmentReportInData(
         ServerRequest $serverRequest,
-        LoggerInterface $logger,
         array $reports,
         array $itemsIdToInfo,
         array $lessonIdToInfo,
@@ -205,9 +223,9 @@ final class FoundAssessmentReport
         $foundReport = [];
         $requestParams = $serverRequest->getQueryParams();
         // Поиск оценок
+        $ReportMeetSearchCriteria = null;
         foreach ($reports as $report) {
             //$ReportMeetSearchCriteria = getSearch($request, $report, $appConfig);
-            $ReportMeetSearchCriteria = null;
             if (array_key_exists(
                 'item_name',
                 $requestParams
@@ -239,137 +257,62 @@ final class FoundAssessmentReport
 
 
             if ($ReportMeetSearchCriteria) { // Отбор наёденных оценок
-                $report['lesson_id'] = $lessonIdToInfo[$report['lesson_id']];
-                $report['student_id'] = $studentIdToInfo[$report['student_id']];
-                $foundReport[] = ReportClass::createFromArray($report);
+                $foundReport[] = $this->ReportFactory($report, $lessonIdToInfo, $studentIdToInfo);
+                $ReportMeetSearchCriteria = null;
             }
         }//Цикл по оценкам [конец]
 
-        $this->Log($logger,'found Report' . count($foundReport));
+        $this->Log($this->logger, 'found Report' . count($foundReport));
         return $foundReport;
     }
 
-
-    private function ReportFactory (): ReportClass
+    /**
+     * Реализация логики создания электронного журнала
+     *
+     * @param array $report - массив оценок
+     * @param array $lessonIdToInfo - Массив сущностей Занятий
+     * @param array $studentIdToInfo - Массив сущностей Студентов
+     * @return ReportClass
+     */
+    private function ReportFactory(array $report, array $lessonIdToInfo, array $studentIdToInfo): ReportClass
     {
-        //TODO Доделать класс поиска оценок, доделать метод отвечающий за реализацию логики создания текствого документа
+        $report['lesson_id'] = $lessonIdToInfo[$report['lesson_id']];
+        $report['student_id'] = $studentIdToInfo[$report['student_id']];
+        return ReportClass::createFromArray($report);
     }
 
     /**
      * Обработка запроса поиска оценок
      *
-     * @param ServerRequest $request - http запрос
-     * @param LoggerInterface $logger - название функции логирования
-     * @param AppConfig $appConfig - конфигурация приложения
+     * @param ServerRequest $serverRequest - http запрос
      * @return HttpResponse - http ответ
      * @throws InvalidDataStructureException
      */
-    public function __invoke(ServerRequest $request, LoggerInterface $logger, AppConfig $appConfig): HttpResponse
+    public function __invoke(ServerRequest $serverRequest): HttpResponse
     {
-        $items = loadData($appConfig->getPathToItems());
-        $teachers = loadData($appConfig->getPathToTeachers());
-        $classes = loadData($appConfig->getPathToClasses());
-        $lessons = loadData($appConfig->getPathToLesson());
-        $reports = loadData($appConfig->getPathToAssessmentReport());
-        $students = loadData($appConfig->getPathToStudents());
-        $parents = loadData($appConfig->getPathToParents());
+        $this->Log($this->logger, 'assessmentReport" url');
+        $resultOfParamValidation = $this->ValidateQueryParams($serverRequest);
+        if (null === $resultOfParamValidation) {
+            $httpCode=200;
+            $itemsIdToInfo = $this->loadItemsEntity();
+            $lessonIdToInfo = $this->loadLessonEntity($itemsIdToInfo);
+            $studentIdToInfo = $this->loadStudentsEntity();
+            $reports = $this->loadReportData();
 
-        $logger->log('assessmentReport" url');
-
-        $paramValidations = [
-            'item_name' => 'Incorrect item name',
-            'item_description' => 'Incorrect item description',
-            'lesson_date' => 'Incorrect lesson date',
-            'student_fio' => 'Incorrect student fio',
-        ];
-        $requestParams = $request->getQueryParams();
-
-        if (null === ($result = paramTypeValidation($paramValidations, $requestParams))) {
-            //Хэшмапирование
-            $foundReport = [];
-            $itemsIdToInfo = [];
-            $teachersIdToInfo = [];
-            $classesIdToInfo = [];
-            $lessonIdToInfo = [];
-            $studentIdToInfo = [];
-            $parentIdToInfo = [];
-
-            foreach ($items as $item) {
-                $itemsObj = ItemClass::createFromArray($item);
-                $itemsIdToInfo[$itemsObj->getId()] = $itemsObj;
-            }
-
-            foreach ($teachers as $teacher) {
-                $teacher['idItem'] = $itemsIdToInfo[$teacher['idItem']];
-                $teachersObj = TeacherUserClass::createFromArray($teacher);
-                $teachersIdToInfo[$teachersObj->getId()] = $teachersObj;
-            }
-
-            foreach ($classes as $class) {
-                $classesObj = ClassClass::createFromArray($class);
-                $classesIdToInfo[$classesObj->getId()] = $classesObj;
-            }
-
-            foreach ($lessons as $lesson) {
-                $lesson['item_id'] = $itemsIdToInfo[$lesson['item_id']];
-                $lesson['teacher_id'] = $teachersIdToInfo[$lesson['teacher_id']];
-                $lesson['class_id'] = $classesIdToInfo[$lesson['class_id']];
-                $lessonsObj = LessonClass::createFromArray($lesson);
-                $lessonIdToInfo[$lessonsObj->getId()] = $lessonsObj;
-            }
-
-            foreach ($parents as $parent) {
-                $parentsObj = ParentUserClass::createFromArray($parent);
-                $parentIdToInfo[$parentsObj->getId()] = $parentsObj;
-            }
-
-            foreach ($students as $student) {
-                $student['class_id'] = $classesIdToInfo[$student['class_id']];
-                $student['parent_id'] = $parentIdToInfo[$student['parent_id']];
-                $studentsObj = StudentUserClass::createFromArray($student);
-                $studentIdToInfo[$studentsObj->getId()] = $studentsObj;
-            }
-
-            // Поиск оценок
-            foreach ($reports as $report) {
-                //$ReportMeetSearchCriteria = getSearch($request, $report, $appConfig);
-                $ReportMeetSearchCriteria = null;
-                if (array_key_exists(
-                    'item_name',
-                    $requestParams
-                )) // Поиск по присутвию item_name в GET запросе и совпадению item_name в запросе и массиве оценок. [начало]
-                {
-                    $ReportMeetSearchCriteria = ($requestParams['item_name'] === $itemsIdToInfo[$lessonIdToInfo[$report['lesson_id']]->getItem(
-                        )->getId()]->getName());
-                }// Поиск по присутвию item_name в GET запросе и совпадению item_name в запросе и массиве оценок. [конец]
-                if (array_key_exists('item_description', $requestParams)) {
-                    $ReportMeetSearchCriteria = ($requestParams['item_description'] === $itemsIdToInfo[$lessonIdToInfo[$report['lesson_id']]->getItem(
-                        )->getId()]->getDescription());
-                }
-                if (array_key_exists('lesson_date', $requestParams)) {
-                    $ReportMeetSearchCriteria = ($requestParams['lesson_date'] === $lessonIdToInfo[$report['lesson_id']]->getDate(
-                        ));
-                }
-                if (array_key_exists('student_fio', $requestParams)) {
-                    $ReportMeetSearchCriteria = ($requestParams['student_fio'] === $studentIdToInfo[$report['student_id']]->getFio(
-                        ));
-                }
-
-
-                if ($ReportMeetSearchCriteria) { // Отбор наёденных оценок
-                    $report['lesson_id'] = $lessonIdToInfo[$report['lesson_id']];
-                    $report['student_id'] = $studentIdToInfo[$report['student_id']];
-                    $foundReport[] = ReportClass::createFromArray($report);
-                }
-            }//Цикл по оценкам [конец]
-
-            $logger->log('found Report' . count($foundReport));
-
+            $result = $this->searchForAssessmentReportInData(
+                $serverRequest,
+                $reports,
+                $itemsIdToInfo,
+                $lessonIdToInfo,
+                $studentIdToInfo
+            );
+        } else {
+            $httpCode = 500;
             $result = [
-                'httpCode' => 200,
-                'result' => $foundReport
+                'status' => 'fail',
+                'message' => $resultOfParamValidation
             ];
         }
-        return ServerResponseFactory::createJsonResponse($result['httpCode'], $result['result']);
+        return ServerResponseFactory::createJsonResponse($httpCode, $result);
     }
 }

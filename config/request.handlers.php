165eccaf4076243de195951bcaad395685747b76
Денис <ivanyuchenko.d.o@effective-group.ru>
2021-12-23 11:11:27 +0300

    Класс поиска занятий сделан. Тесты все проходят

--- config/request.handlers.php
+++ config/request.handlers.php
@@ -13,14 +13,14 @@ return [
         LoggerInterface $logger,
         AppConfig $appConfig
     ): HttpResponse {
-        return (new Controller\FoundAssessmentReport($logger, $appConfig))($serverRequest);
+        return (new Controller\FoundLesson($logger, $appConfig))($serverRequest);
     },
     '/assessmentReport' => static function (
         ServerRequest $serverRequest,
         LoggerInterface $logger,
         AppConfig $appConfig
     ): HttpResponse {
-        return (new Controller\FoundLesson($logger, $appConfig))($serverRequest);
+        return (new Controller\FoundAssessmentReport($logger, $appConfig))($serverRequest);
     },
 
     '/studentReport' => static function (

c2bbb652d7bca7dc39fd5e79d2233762b4fd5ec7
shorkin.d.a <shorkin.d.a@effective-group.ru>
2021-12-26 17:34:32 +0300

    Переделал хендлерсы

--- config/request.handlers.php
+++ config/request.handlers.php
@@ -1,33 +1,13 @@
 <?php
 
 use JoJoBizzareCoders\DigitalJournal\Controller;
-use JoJoBizzareCoders\DigitalJournal\Infrastructure\AppConfig;
-use JoJoBizzareCoders\DigitalJournal\Infrastructure\Http\ServerRequest;
-use JoJoBizzareCoders\DigitalJournal\Infrastructure\Logger\LoggerInterface;
-use JoJoBizzareCoders\DigitalJournal\Infrastructure\Http\HttpResponse;
+
 
 
 return [
-    '/lesson'  => static function (
-        ServerRequest $serverRequest,
-        LoggerInterface $logger,
-        AppConfig $appConfig
-    ): HttpResponse {
-        return (new Controller\FoundLesson($logger, $appConfig))($serverRequest);
-    },
-    '/assessmentReport' => static function (
-        ServerRequest $serverRequest,
-        LoggerInterface $logger,
-        AppConfig $appConfig
-    ): HttpResponse {
-        return (new Controller\FoundAssessmentReport($logger, $appConfig))($serverRequest);
-    },
+    '/lesson'  => Controller\FoundLesson::class,
 
-    '/studentReport' => static function (
-        ServerRequest $serverRequest,
-        LoggerInterface $logger,
-        AppConfig $appConfig
-    ): HttpResponse {
-        return (new Controller\FoundAssessmentReport($logger, $appConfig))($serverRequest);
-    },
+    '/assessmentReport' => Controller\FoundAssessmentReport::class,
+
+    '/studentReport' => Controller\FoundAssessmentReport::class
 ];

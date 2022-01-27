3fff73c41cdca63b8f0bc7390b650df7f7bb1cfe
shorkin.d.a <shorkin.d.a@effective-group.ru>
2022-01-27 20:24:04 +0300

    Комит композеров

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use JoJoBizzareCoders\DigitalJournal\Config\AppConfig;
 use JoJoBizzareCoders\DigitalJournal\Infrastructure\Autoloader\Autoloader;

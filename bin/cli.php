0387137b65c98853aedaca14fee26fb8f10532fe
shorkin.d.a <shorkin.d.a@effective-group.ru>
2022-02-16 10:52:55 +0300

    Логирование в телеграмм работает, изменил di.php на di.xml в консольной версии приложения

--- bin/cli.php
+++ bin/cli.php
@@ -2,11 +2,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use JoJoBizzareCoders\DigitalJournal\Config\ContainerExtensions;
 use JoJoBizzareCoders\DigitalJournal\Infrastructure\Console\AppConsole;
 use JoJoBizzareCoders\DigitalJournal\Infrastructure\Console\Output\OutputInterface;
-use JoJoBizzareCoders\DigitalJournal\Infrastructure\DI\Container;
 use JoJoBizzareCoders\DigitalJournal\Infrastructure\DI\ContainerInterface;
-
+use JoJoBizzareCoders\DigitalJournal\Infrastructure\DI\SymfonyDiContainerInit;
 
 
 (new AppConsole(
@@ -14,8 +14,19 @@ use JoJoBizzareCoders\DigitalJournal\Infrastructure\DI\ContainerInterface;
     static function (ContainerInterface $di): OutputInterface {
         return $di->get(OutputInterface::class);
     },
-    static function (): ContainerInterface {
-        return Container::createFromArray(require __DIR__ . '/../config/dev/di.php');
-    }
+    new SymfonyDiContainerInit(
+        new SymfonyDiContainerInit\ContainerParams(
+            __DIR__ . '/../config/dev/di.xml',
+            [
+                'kernel.project_dir' => __DIR__ . '/../'
+            ],
+            ContainerExtensions::httpAppContainerExtensions()
+        ),
+        new SymfonyDiContainerInit\CacheParams(
+            //'DEV' !== getenv('ENV_TYPE'),
+                false,
+            __DIR__ . '/../var/cache/di-symfony/DigitalJournalCachedContainer.php',
+        )
+    ),
 ))->dispatch();
 
